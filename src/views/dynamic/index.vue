99e9cdf0d273cf66f5c3d023c2baf7c045aaeec2
LPX <lpxuan1997@163.com>
2021-11-08 15:46:33 +0800

    初始化

---
+++ src/views/dynamic/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'dynamic',
+
+  data () {
+    return {}
+  },
+
+  mounted () {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/layout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WeiaiMLayout',
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
